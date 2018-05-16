e5f9c5d54ea2c81595ce63d2328d21c84cf103e6
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-16 13:09:33 +0300

    q

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,5 @@ var name=prompt("Enter your favourite color");
 alert(name " is a color for bright");
 var Age=prompt("How Old are you?");
 if(age<18){
-    alert("please look for a ")
+    alert("please look for a cartoon")
 }
