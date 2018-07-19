20c170bac7d88fd21dc69bfc9cb15845f264ad17
mohammed yusuf <mwaigalo5@gmail.com>
2018-07-19 19:33:06 +0300

    G

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
-var name=prompt("Enter your favourite color");
-alert(name " is a color for bright");
-var Age=prompt("How Old are you?");
-if(age<18){
-    alert("PLEASE look for a cartoon network site")
-}
+//var name=prompt("Enter your favourite color");
+//alert(name " is a color for bright");
+//var Age=prompt("How Old are you?");
+//if(age<18){
+//    alert("PLEASE look for a cartoon network site")
+//}
