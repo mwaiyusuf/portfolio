aeab62e19d10d88200bc2b86f15d5f351e946544
mohammed yusuf <mwaigalo5@gmail.com>
2018-08-16 23:43:15 +0300

    e

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,11 @@
 //if(age<18){
 //    alert("PLEASE look for a cartoon network site")
 //}
+ function validateEmail() {
+2   var eEntered = document.getElementById("address").value;
+3   var regex = /^[\w\-\.\+]+\@[a-zA-Z0-9\. \-]+\.[a-zA-z0-9]{2,4}$/;
+4   if (!(eEntered.match(emailCorrectPattern))) {
+5     alert("Please correct email address");
+6     return false; 
+   }
+ } 
