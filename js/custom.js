0e1e7116bff162fed22d484785707d71da68ee32
mwaiyusuf <mwaigalo5@gmail.com>
2018-11-01 20:42:43 +0300

    t

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 var name=prompt("Enter your favourite color");
-alert(name " is a color for bright");
+//alert(name " is a color for bright");
 //var Age=prompt("How Old are you?");
 //if(age<18){
 //    alert("PLEASE look for a cartoon network site")
